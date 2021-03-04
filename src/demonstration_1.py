68a36810ef9b08763c60eb87f88c64df5c5a0107
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2021-03-03 21:44:06 -0500

    notes in demo

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -16,7 +16,7 @@ alphabet. In other words, my list of names is sorted, but it is "rotated."
 Example:
 
 surnames = [
-    'liu',
+    'liu',    ##Floor
     'mcdowell',
     'nixon',
     'sparks',
@@ -26,7 +26,7 @@ surnames = [
     'davenport',
     'farley',
     'glover',
-    'kennedy',
+    'kennedy',  ##Ceiling
 ]
 
 Write a function that finds the index of the "rotation point". The "rotation
@@ -35,7 +35,37 @@ list I came up was absolutely huge, so make sure your solution is efficient.
 
 *Note: you should be able to come up with a solution that has O(log n) time
 complexity.*
+
+-What is a rotation point? 
+-where zhang meets ahmed
+- sparks has a bigger value than liu
+- guessing in the middle at sparks
+- as long as value point is getting higher (alphabetical) then it's not the rotation point
+
+guess in middle
+    if guess >= floor name:
+        #go right
+        ceiling = guess index
+    if guess < floor name:
+        ceiling = guess
+    if floor + 1 == ceiling:
+        return ceiling
 """
 def find_rotation_point(surnames):
-    # Your code here
+    # create a floor index at 0
+    # create a ceiling index at length- 1
+    # keep track of first_name in our window
+    # first_name = surname[floor_index]
+
+    #while floor_index < ceiling_index:
+        #guess in middle of floor & ceiling
+        #guess = ceiling + floor // 2 (get middle index)
+        # if the word at guess_index is larger than first_name
+            #shrink our floor down to guess_index
+        #else
+            #shrink our ceiling down to the guess_index
+
+        #if we rech a point, where there's only two items left between
+        # ceiling and floor, returnthe item at the ceiling, that item will
+        # always be alphabetically smaller
 
